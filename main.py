cdb18895245845e9718632c9019ebe1198dfeec0
Peter Perry <99445714+Lact0@users.noreply.github.com>
2022-11-01 17:09:05 +0000

    FNN

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from nn import *
 
-l = nodeLayer(1, 1)
+l1 = nodeLayer(1, 20, sigmoid)
+l2 = nodeLayer(20, 1, sigmoid)
+lr = .01
+n = 2
+f = lambda x: sin(x)
 
+net = FNN(1, [20, 1], [sigmoid, sigmoid], ssr)
 
-def f(x): return 3 * x + 2
+def trainBatch():
+  gradients = []
+  AvgErr = 0
+  for i in range(n):
+    x = random() * 3.1415926535897 * 2
+    gradient = []
+    out1 = l1.feed([x])
+    out2 = l2.feed(out1)
+    outGrad = ssr[1](out2, [f(x)])
+    AvgErr += ssr[0](out2, [f(x)])
+    gradient += [l2.getWeightGradient(outGrad, out1)]
+    outGrad = l2.getInputGradient(outGrad, out1)
+    gradient = [l1.getWeightGradient(outGrad, [f(x)])] + gradient
+    gradients += [gradient]
+  gradient = gradientMean(gradients)
+  l1.applyGradient(gradient[0], lr)
+  l2.applyGradient(gradient[1], lr)
+  print('Avg err: ', AvgErr / n)
 
+def train():
+  for i in range(100000):
+    trainBatch()
 
-for i in range(100000):
-    x = randint(-100, 100)
-    output = l.feed([x])[0]
-    error = (f(x) - output) ** 2
-    print(error)
-    l.train([-2 * (f(x) - output)], [x], .000001)
-    print(.1 ** log(error))
+train()
--- nn.py
+++ nn.py
@@ -7,6 +7,22 @@ sigmoid = [lambda x: 1 / (1 + exp(-x)), lambda x: (1 / (1 + exp(-x))) * (1 - (1 
 relu = [lambda x: max(0, x), lambda x: 0 if x < 0 else 1]
 none = [lambda x: x, lambda x: 1]
 
+#Error Functions
+ssr = [lambda pred, ans: sum([(ans[i] - pred[i]) ** 2 for i in range(len(pred))]), lambda pred, ans: [-2 * (ans[i] - pred[i]) for i in range(len(pred))]]
+
+
+def gradientMean(gradients):
+  ret = []
+  for i in range(len(gradients[0])):
+    temp1 = []
+    for j in range(len(gradients[0][i])):
+      temp2 = []
+      for k in range(len(gradients[0][i][j])):
+        total = [gradient[i][j][k] for gradient in gradients]
+        temp2.append(sum(total) / len(gradients))
+      temp1.append(temp2)
+    ret.append(temp1)
+  return ret
 
 class nodeLayer:
     def __init__(this, numIn, width, actFunc=none, weights=False):
@@ -16,7 +32,7 @@ class nodeLayer:
         if not weights:
             for i in range(width):
                 temp = []
-                for i in range(numIn + 1):
+                for j in range(numIn + 1):
                     temp += [random() * 2 - 1]
                 this.weights += [temp]
         else:
@@ -29,6 +45,7 @@ class nodeLayer:
             total = [inp[i] * weights[i] for i in range(this.numIn + 1)]
             sm = sum(total)
             out.append(this.actFunc[0](sm))
+        inp.pop()
         return out
 
     def getWeightGradient(this, outputGradient, inp):
@@ -42,10 +59,60 @@ class nodeLayer:
             for j in range(len(weights)):
                 temp.append(prev * inp[j])
             ret.append(temp)
-            return ret
+        inp.pop()
+        return ret
 
-    def train(this, outputGradient, inp, lr):
-        gradient = this.getWeightGradient(outputGradient, inp)
+    def applyGradient(this, gradient, lr):
         for i in range(len(gradient)):
             for j in range(len(gradient[i])):
                 this.weights[i][j] -= gradient[i][j] * lr
+
+    def getInputGradient(this, outputGradient, inp):
+        inp.append(1)
+        ret = [0] * len(inp)
+        for x in range(len(this.weights)):
+            weights = this.weights[x]
+            sm = sum([inp[i] * weights[i] for i in range(len(inp))])
+            grad = outputGradient[x] * this.actFunc[1](sm)
+            for i in range(len(inp)):
+                ret[i] += grad * weights[i]
+        inp.pop()
+        return ret
+
+class FNN:
+  def __init__(this, numIn, dim, actFuncs, errFunc):
+    this.numIn = numIn
+    this.dim = dim
+    this.layers = []
+    this.errFunc = errFunc
+    for i in range(len(dim)):
+      nIn = dim[i - 1] if i > 0 else numIn
+      lay = nodeLayer(nIn, dim[i], actFuncs[i])
+      this.layers += [lay]
+  
+  def feed(this, inp):
+    nextIn = inp
+    for layer in this.layers:
+      nextIn = layer.feed(nextIn)
+    return nextIn
+
+  def getWeightGradient(this, inp, ans):
+    inputs = [inp]
+    for layer in this.layers:
+      inputs += [layer.feed(inputs[-1])]
+    weightGradient = []
+    errGrad = this.errFunc[1](inputs[-1], ans)
+    for i in range(len(this.layers) - 1, -1, -1):
+      layer = this.layers[i]
+      outGrad = 0
+      if len(weightGradient) == 0:
+        outGrad = errGrad
+      else:
+        outGrad = weightGradient[0]
+      weightGradient = [layer.getWeightGradient(outGrad, inputs[i])] + weightGradient
+    return weightGradient
+
+  def applyGradient(this, grad, lr):
+    for i in range(len(this.layers)):
+      layer = this.layers[i]
+      layer.applyGradient(grad[i], lr)
